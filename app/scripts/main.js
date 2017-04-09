586d8a3dd51ae6f6e10a1d27768484f57399da05
John Britti <jakebritti@gmail.com>
2017-04-09 16:31:10 -0400

    snazzy UI and question flipping

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,35 @@
-$('body').scrollspy({
-    target: '.bs-docs-sidebar',
-    offset: 40
+var current = null;
+var next = "q1";
+var questions = ["q1", "q2", "q3", "q4", "q5", "q6"]
+
+$(document).ready(function() {
+	$('#start-button').on('click', function() {
+		nextQuestion();
+		$('.intro').toggleClass('done');
+	});
+	$('#next').on('click', function() {
+		nextQuestion();
+	});
+	$('#prev').on('click', function() {
+		prevQuestion();
+	});
 });
+
+function nextQuestion() {
+	$('#' + current).toggleClass('current')
+	current = getNext(current);
+	$('#' + current).toggleClass('current')
+}
+
+function prevQuestion() {
+	$('#' + current).toggleClass('current')
+	current = getPrev(current);
+	$('#' + current).toggleClass('current')
+}
+
+function getNext(q) {
+	return questions[questions.indexOf(q) + 1];
+}
+function getPrev(q) {
+	return questions[questions.indexOf(q) - 1];
+}
