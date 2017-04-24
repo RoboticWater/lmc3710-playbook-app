eb8ef63ff19f5f214381d408584c6b72b7e42547
John Britti <jakebritti@gmail.com>
2017-04-23 22:50:48 -0400

    added python 3 program that will calculate totals

---
+++ app/resources/calc_totals.py
@@ -0,0 +1,21 @@
+import json
+jo = json.load(open("questions.json"))
+group_totals, play_totals = {},{}
+
+for question in jo:
+    if jo[question]["group"] not in group_totals.keys():
+        group_totals[jo[question]["group"]] = max(jo[question]["values"])
+    else:
+        group_totals[jo[question]["group"]] += max(jo[question]["values"])
+    if jo[question]["subplay"] not in play_totals.keys():
+        play_totals[jo[question]["subplay"]] = max(jo[question]["values"])
+    else:
+        play_totals[jo[question]["subplay"]] += max(jo[question]["values"])
+
+for k, v in group_totals.items():
+    print('"%s": %d,' % (k, v))
+
+print()
+
+for k, v in play_totals.items():
+    print('"%s": %d,' % (k, v))
