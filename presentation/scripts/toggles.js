9dbdbdad9b58b3c1b900c436bdb7a2b9a4d553b6
RoboticWater <jakebritti@gmail.com>
2017-04-24 16:34:17 -0400

    final commit

--- presentation/scripts/toggles.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('#toggle-nav').on('click', function() {
-		$('body').toggleClass('hide-leftbar');
-	});
-	$('#toggle-graph').on('click', function() {
-		$('body').toggleClass('hide-rightbar');
-	});
-});
